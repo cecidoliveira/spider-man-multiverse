6fd6b06cd82570156d4045392049a973356959eb
cecidoliveira <cidornelas.pb@gmail.com>
2024-10-24 13:15:52 -0300

    [feat] create scripts, styles

---
+++ src/scripts/cardHoverAnimation.js
@@ -0,0 +1,20 @@
+const addEventListenersToCards = () => {
+    const cardElements = document.getElementsByClassName('s-card');
+
+    for( let i = 0; i < cardElements.length; i++){
+        const card = cardElements[i];
+        card.addEventListener('mouseenter', handleMouseEnter);
+        card.addEventListener('mouseleave', handleMouseLeave);
+    }
+}
+
+function handleMouseEnter () {
+    this.classList.add('s-card--hovered');
+    document.body.id = `${this.id}--hovered`;
+}
+function handleMouseLeave () {
+    this.classList.remove('s-card--hovered');
+    document.body.id = '';
+}
+
+document.addEventListener("DOMContentLoaded", addEventListenersToCards)
